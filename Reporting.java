1d7092568c250187e25cbe174a31f1f6f51d9c10
Madhavgarg-mg <c2064676@newcastle.ac.uk>
2023-02-06 19:04:37 +0530

    Added highestItemPrice()

--- Reporting.java
+++ Reporting.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+
 public class Reporting {
+    public AuctionHouse[] auctionHouses;
+
+    public Reporting(AuctionHouse[] auctionHouses) {
+        this.auctionHouses = auctionHouses;
+    }
+    public String stringReporting(){
+        return String.format("""
+                Auction houses:\t%s
+                """, Arrays.toString(auctionHouses));
+    }
+    double highestPrice;
+    public double highestItemPrice() {
+        for (int i = 0; i < auctionHouses.length; i++) {
+            for(int j = 0; j < auctionHouses[i].items.length; j++){
+                if (auctionHouses[i].items[j].price > highestPrice){
+                    highestPrice = auctionHouses[i].items[j].price;
+                }
+            }
+        }
+        return highestPrice;
+    }
 }
