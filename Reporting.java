a3cd641f8c868c5ec0049ddf9f5530d08ed83801
Madhav Garg <c2064676@newcastle.ac.uk>
2023-02-07 13:16:15 +0000

    Changed auctionHouses to be a list

--- Reporting.java
+++ Reporting.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Reporting {
-    public AuctionHouse[] auctionHouses;
+    List<AuctionHouse> auctionHouses = new ArrayList<>();
 
-    public Reporting(AuctionHouse[] auctionHouses) {
-        this.auctionHouses = auctionHouses;
+    public Reporting() {
+
     }
-    public String stringReporting(){
-        return String.format("""
-                Auction houses:\t%s
-                """, Arrays.toString(auctionHouses));
+
+    public String toString() {
+        return "Reporting{" +
+                "auctionHouse=" + auctionHouses +
+                '}';
     }
+
+    public void addAuctionHouse(AuctionHouse house){
+        auctionHouses.add(house);
+    }
+
     double highestPrice;
     public double highestItemPrice() {
-        for (int i = 0; i < auctionHouses.length; i++) {
-            for(int j = 0; j < auctionHouses[i].items.size(); j++){
-                if (auctionHouses[i].items.get(j).getPrice() > highestPrice){
-                    highestPrice = auctionHouses[i].items.get(j).getPrice();
+        for (int i = 0; i < auctionHouses.size(); i++) {
+            for(int j = 0; j < auctionHouses.get(i).items.size(); j++){
+                if (auctionHouses.get(i).items.get(j).getPrice() > highestPrice){
+                    highestPrice = auctionHouses.get(i).items.get(j).getPrice();
                 }
             }
         }
@@ -28,19 +35,19 @@ public class Reporting {
     String nameHighestAverage;
 
     public String highestAveragePriceYear(int year) {
-        for (int i = 0; i < auctionHouses.length; i++) {
+        for (int i = 0; i < auctionHouses.size(); i++) {
             double averagePrice = 0;
             int totalItemsInYear = 0;
-            for(int j = 0; j < auctionHouses[i].items.size(); j++){
-                if (auctionHouses[i].items.get(j).getYear() == year){
-                        averagePrice += auctionHouses[i].items.get(j).getPrice();
+            for(int j = 0; j < auctionHouses.get(i).items.size(); j++){
+                if (auctionHouses.get(i).items.get(j).getYear() == year){
+                        averagePrice += auctionHouses.get(i).items.get(j).getPrice();
                         totalItemsInYear += 1;
                 }
                 }
             averagePrice = averagePrice / totalItemsInYear;
             if (averagePrice > highestAveragePrice){
                 highestAveragePrice = averagePrice;
-                nameHighestAverage = auctionHouses[i].getName();
+                nameHighestAverage = auctionHouses.get(i).getName();
             }
         }
         return nameHighestAverage;
@@ -48,10 +55,10 @@ public class Reporting {
 
     ArrayList<Integer> itemGreater = new ArrayList<>();
     public ArrayList<Integer> listGreaterThanAmount(double amount) {
-        for (int i = 0; i < auctionHouses.length; i++) {
-            for(int j = 0; j < auctionHouses[i].items.size(); j++){
-                if (auctionHouses[i].items.get(j).getPrice() > amount){
-                    itemGreater.add(auctionHouses[i].items.get(j).getItemLot());
+        for (int i = 0; i < auctionHouses.size(); i++) {
+            for(int j = 0; j < auctionHouses.get(i).items.size(); j++){
+                if (auctionHouses.get(i).items.get(j).getPrice() > amount){
+                    itemGreater.add(auctionHouses.get(i).items.get(j).getItemLot());
                 }
             }
         }
