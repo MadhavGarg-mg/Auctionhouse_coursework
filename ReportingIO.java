ae637e701e7de82b66f6bb2fdb36210369567a1b
Madhav Garg <c2064676@newcastle.ac.uk>
2023-02-07 13:17:31 +0000

    Added case 3 which prints the auction house with the largest average item price for a specific year

--- ReportingIO.java
+++ ReportingIO.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ReportingIO {
@@ -6,10 +9,14 @@ public class ReportingIO {
     }
 
     public void run() {
+        List<AuctionHouse> auctionHouses = new ArrayList<>();
+
+        Reporting reporting = new Reporting();
         boolean quit = false;
         Scanner sc = new Scanner(System.in);
 
         while(!quit){
+
             Scanner input = new Scanner(System.in);
             String options = """ 
                 1. Enter Auction house data.
@@ -24,16 +31,26 @@ public class ReportingIO {
             System.out.println(options);
             int choice = sc.nextInt();
 
-
             switch (choice){
                 case 1:
-                    System.out.println("Please enter the Auction house name: ");
-                    String name = sc.next();
-                    System.out.println("Please enter the items sold in the Auction house: ");
 
-//                    AuctionHouse scname = new AuctionHouse(name, );
+                    System.out.println("How many houses do you want to add? ");
+                    Scanner numHouses = new Scanner(System.in);
+
+                    int numberAuctionHouses = numHouses.nextInt();
+                    for (int i = 0; i < numberAuctionHouses; i++){
+                        System.out.println("Please enter the Auction House name");
+                        String name = sc.next();
+                        AuctionHouse auctionHouseName = new AuctionHouse(name);
+                        auctionHouses.add(auctionHouseName);
+                        reporting.addAuctionHouse(auctionHouseName);}
+                    System.out.println(auctionHouses);
+
+
                     break;
+
                 case 2:
+
                     System.out.println("Please enter the item lot number: ");
                     int itemLot = sc.nextInt();
 
@@ -49,13 +66,32 @@ public class ReportingIO {
                     System.out.println("Please enter the item type: ");
                     String itemType = sc.next();
 
-                    Item item = new Item(itemLot, buyerName, price, year, itemType);
+                    System.out.println("What auction house do you want to add these items to? ");
 
+                    Scanner auctionHouse = new Scanner(System.in);
+
+                    Item itemInformation = new Item(itemLot, buyerName, price, year, itemType);
+
+                    String auctionName = auctionHouse.next();
+
+                    for (int j = 0; j < auctionHouses.size(); j++){
+                        if (Objects.equals(auctionHouses.get(j).getName(), auctionName)){
+                            auctionHouses.get(j).addItem(itemInformation);}
+                    }
+
+                    System.out.println(auctionHouses);
                     break;
+
                 case 3:
-                    System.out.println("a");
+                    System.out.println("For which year would you like to get the auction house with the largest " +
+                            "average item price");
+                    Scanner years = new Scanner(System.in);
+                    System.out.println(reporting.highestAveragePriceYear(years.nextInt()));
 
                     break;
+
+                case 4:
+
 //                case 4:
 //                    System.out.println("Quitting");
 //                    quit = true;
